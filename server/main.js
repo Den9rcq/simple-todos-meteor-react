39f212f02ccc7100cc11f7eee2e2329f02d446e3
Denis Bugreev <den9.rcq@gmail.com>
2022-03-30 14:23:46 +0300

    init tasks collection

--- server/main.js
+++ server/main.js
@@ -1,9 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from '/imports/api/TasksCollection';
 
-function insertLink({ title, url }) {
-
-}
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
- 
+  if (TasksCollection.find().count() === 0) {
+    [
+      'Make physical charging',
+      'To take a shower',
+      'Have breakfast',
+      'English lesson',
+      'Programming lesson',
+      'Dinner',
+      'Programming lesson'
+    ].forEach(insertTask)
+  }
 });
